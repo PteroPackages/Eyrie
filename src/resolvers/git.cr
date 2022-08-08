83f36fad06fd6168cf39c3479f10c79ee7144cb2
devnote-dev <inferno0745@gmail.com>
2022-08-08 03:00:04 +0100

    refactor(resolvers): reduce to git resolver

--- src/resolvers/git.cr
+++ src/resolvers/git.cr
@@ -2,5 +2,19 @@ require "./resolver"
 
 module Eyrie
   class GitResolver < Resolver
+    def self.run(mod : ModuleSpec) : Bool
+      path = cache_path / mod.name
+      return false unless rewrite path
+
+      done = false
+      3.times do
+        next if done
+        Log.vinfo { "cloning: #{mod.source.url}" }
+        done = exec "git clone -c core.askPass=true #{mod.source.url} #{path}"
+      end
+
+      Log.error { "failed to clone repository for '#{mod.name}'" } unless done
+      done
+    end
   end
 end
