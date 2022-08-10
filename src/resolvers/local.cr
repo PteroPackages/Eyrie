1a8e848bd55d608c1e0cf5853096e22c36ad2353
devnote-dev <inferno0745@gmail.com>
2022-08-10 16:02:54 +0100

    revert(resolvers): remove local resolver

--- src/resolvers/local.cr
+++ src/resolvers/local.cr
@@ -2,29 +2,5 @@ require "./resolver"
 
 module Eyrie
   class LocalResolver < Resolver
-    def self.run(spec : ModuleSpec) : Bool
-      src = Path[spec.source.url]
-      unless File.exists? src
-        Log.error { "invalid file path for module '#{spec.name}'" }
-        return false
-      end
-
-      mod = uninitialized Module
-      begin
-        data = File.read src
-        mod = ModuleSpec.from_yaml data
-      rescue ex
-        Log.error(ex) { "failed to read module file" }
-        return false
-      end
-
-      begin
-        mod.validate
-      rescue ex
-        Log.error(ex) { }
-      end
-
-      true
-    end
   end
 end
