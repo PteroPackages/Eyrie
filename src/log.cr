e0358591c8807812f7c850639ea4c30d5a41d8a1
devnote-dev <inferno0745@gmail.com>
2022-09-22 16:11:19 +0100

    refactor(log): optimizations

--- src/log.cr
+++ src/log.cr
@@ -1,90 +1,103 @@
 module Eyrie::Log
+  extend self
+
+  enum Status
+    INVALID_NAME
+    INVALID_VERSION
+    INVALID_SUPPORTS
+    NO_FILES
+  end
+
   @@trace = false
   @@verbose = false
   @@warn = true
 
-  def self.no_color : Nil
-    Colorize.enabled = false
+  def configure(options : CLI::OptionsInput) : Nil
+    Colorize.enabled = false if options.has? "no-color"
+    @@trace = true if options.has? "trace"
+    @@verbose = true if options.has? "verbose"
+    # @@warn = false if options.has? "no-warn"
   end
 
-  def self.trace : Nil
-    @@trace = true
+  def get_message(status : Status) : Array(String)
+    case status
+    in Status::INVALID_NAME
+      ["module name is invalid", "module name can only contain letters, numbers, dashes and underscores"]
+    in Status::INVALID_VERSION
+      ["invalid version format", "module versions must be in the major.minor.patch format"]
+    in Status::INVALID_SUPPORTS
+      ["invalid supported version", "supported version must be in the major.minor.patch format"]
+    in Status::NO_FILES
+      ["no files were specified to install with the module", "cannot guess which files to install"]
+    end
   end
 
-  def self.no_warnings : Nil
-    @@warn = false
+  def write(args : String) : Nil
+    STDOUT << args
   end
 
-  def self.verbose : Nil
-    @@verbose = true
-  end
-
-  def self.info(args : String)
+  def info(args : String) : Nil
     STDOUT.puts args
   end
 
-  def self.info(args : Array(String))
-    STDOUT.puts args.join('\n')
+  def info(args : Array(String)) : Nil
+    STDOUT.puts args.join '\n'
   end
 
-  def self.vinfo(args : String)
-    STDOUT.puts args if @@verbose
-  end
-
-  def self.vinfo(args : Array(String))
+  def vinfo(args : _) : Nil
     info args if @@verbose
   end
 
-  def self.warn(args : String)
+  def warn(args : String) : Nil
     return unless @@warn
-    STDOUT.puts "warn".colorize(:yellow).to_s + ": #{args}"
+    STDOUT.puts %(#{"warn".colorize(:yellow)}: #{args})
   end
 
-  def self.warn(args : Array(String))
+  def warn(args : Array(String)) : Nil
     args.each { |a| warn a }
   end
 
-  def self.warn(ex : Exception, args : String | Array(String))
+  def warn(ex : Exception, args : _) : Nil
     warn args
     warn "source: " + ex.message.to_s
     ex.backtrace.each { |t| warn t } if @@trace
   end
 
-  def self.vwarn(args : String | Array(String))
+  def vwarn(args : _) : Nil
     warn args if @@verbose
   end
 
-  def self.vwarn(ex : Exception)
+  def vwarn(ex : Exception) : Nil
     return unless @@verbose && @@warn
     warn ex.message || "an unknown error occured"
     ex.backtrace.each { |t| warn t } if @@trace
   end
 
-  def self.error(args : String)
-    STDERR.puts "error".colorize(:red).to_s + ": #{args}"
+  def error(args : String) : Nil
+    STDERR.puts %(#{"error".colorize(:red)}: #{args})
   end
 
-  def self.error(args : Array(String))
+  def error(args : Array(String)) : Nil
     args.each { |a| error a }
   end
 
-  def self.error(ex : Exception)
+  def error(ex : Exception) : Nil
     error ex.message || "an unknown error occured"
     ex.backtrace.each { |t| error t } if @@trace
   end
 
-  def self.error(ex : Exception, args : String | Array(String))
+  def error(ex : Exception, args : _) : Nil
     error args
     error ex.message.not_nil! if ex.message
     ex.backtrace.each { |t| error t } if @@trace
   end
 
-  def self.fatal(args : String | Array(String) | Exception)
+  def fatal(args : _) : Nil
     error args
     exit 1
   end
 
-  def self.fatal(ex : Exception, args : String | Array(String))
+  def fatal(ex : Exception, args : _) : Nil
     error ex, args
     exit 1
   end
