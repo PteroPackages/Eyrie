72c6052f2519648a4267e86ea79be34c25226a19
devnote-dev <inferno0745@gmail.com>
2022-12-30 17:30:20 +0000

    chore(commands): formatting

--- src/commands/base.cr
+++ src/commands/base.cr
@@ -2,14 +2,14 @@ module Eyrie::Commands::Base
   def on_invalid_options(options)
     Log.fatal [
       "Invalid option#{"s" if options.size > 1} '#{options.join("', '")}'",
-      "See 'eyrie #{self.name} --help' for more information"
+      "See 'eyrie #{self.name} --help' for more information",
     ]
   end
 
   def on_missing_arguments(args)
     Log.fatal [
       "Missing required argument#{"s" if args.size > 1} #{args.join(", ")}",
-      "See 'eyrie #{self.name} --help' for more information"
+      "See 'eyrie #{self.name} --help' for more information",
     ]
   end
 end
